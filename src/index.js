fb93b078b9c7cab989a2313f6f3ae343b78bd20a
Ryan Calingo <gryan.calingo@gmail.com>
2022-10-24 11:26:41 -0400

    card component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './component/App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <HashRouter>
+    <Routes>
+    <Route path="/" element={<App/>} />
+    </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
